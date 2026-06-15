9f2958b23fb864538c8108eb8f0b7bba453be48f
ironerae <ironeraeabis@protonmail.com>
2026-06-15 22:38:26 +0800

    feat: make VorCore 10 project public in Projects component

--- app/Projects.tsx
+++ app/Projects.tsx
@@ -55,7 +55,7 @@ const projects = [
     projectDescription:
       "A custom-designed, open-source CoreXY kinematic mounting system. It bridges the gap between the heavy-duty front-motor layout of the SolidCore and the clean, stacked belt routing made popular by Voron designs.",
     projectStacks: [] as unknown[] as IconType[],
-    isPublic: false,
+    isPublic: true,
     sourceCodeLink: "https://github.com/ironerae/VorCore-10",
     imageSrc: "/images/projects/vorcore-10.png",
     isHighlight: true,
